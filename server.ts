0172f00cd546de7dc03d23c03f1cd13d8e9d8257
artur_fjodorov <artur.fodorov@gmail.com>
2020-07-07 17:56:36 +0300

    minor clean up

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import {AppConfiguration} from './configs/AppConfiguration.ts';
-import router from './src/items/items.controller.ts';
 import {Logger} from './server-configs/Logger.ts';
 import {Routes} from './server-configs/Routes.ts';
 
@@ -11,8 +10,5 @@ for(const router of await Routes.getConfiguredRoutes()) {
   app.use(router.routes());
   app.use(router.allowedMethods());
 }
-
-
-app.use(router.routes());
 
 await app.listen({port: AppConfiguration.server.port});
